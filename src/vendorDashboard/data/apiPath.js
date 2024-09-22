850918e0c8f6501988c5ed5e3a8e4f2559a73307
Vinitcodeit <vtambralli412@gmail.com>
2024-09-22 14:09:05 +0530

    updated

--- src/vendorDashboard/data/apiPath.js
+++ src/vendorDashboard/data/apiPath.js
@@ -1,5 +1,5 @@
 // export const API_URL = "http://localhost:4000"
 
-// export const API_URL = "https://foodzone-backend-nodejs.onrender.com"
-export const API_URL = "https://foodzonenodejsbackendd-cyi2kf55.b4a.run/"
+export const API_URL = "https://foodzone-backend-nodejs.onrender.com"
+// export const API_URL = "https://foodzonenodejsbackendd-cyi2kf55.b4a.run/"
 
